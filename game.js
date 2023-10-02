868d8bca7d79fc76c2177dfa9774302c65caa283
vivek thorat <vivekthorat588@gmail.com>
2023-10-02 11:41:55 +0530

    New updates in Projects

--- game.js
+++ game.js
@@ -7,42 +7,77 @@ var started = false;
 $(document).keypress(function(){
 
   if(!started){
-    $("#title").text("level " + level);
+    $("#title").text("Level " + level);
     nextsequence();
     started = true;
   }
 });
 
-$(".btn").onclick(function(){
+$(".btn").click(function(){
   
-  var usercolour = $(this).attr("id");
-  userclickedpattern.push(usercolour);
-  playsound(usercolour);
-  animatepress(usercolour);
+  var userColour = $(this).attr("id");
+  userclickedpattern.push(userColour);
+  playsound(userColour);
+  animatepress(userColour);
 
+  checkanswer(userclickedpattern.length-1);
 });
+
+function checkanswer(currentLevel){
+
+  if(gamePattern[currentLevel] === userclickedpattern[currentLevel]){
+   if(userclickedpattern.length === gamePattern.length){
+     setTimeout(function(){
+      nextsequence();
+     },1000);
+
+    }
+    }
+    else{
+      playsound("wrong");
+      $("body").addClass("game-over");
+      $("#title").text("Press K To Restart 🤦‍♂️🤦‍♂️");
+
+      setTimeout(function(){
+        $("body").removeClass("game-over");
+      },200);
+
+      startover();
+
+    }
+   }
 
 function nextsequence(){
 
-  $("#title").text("level " + level);
+  userclickedpattern = [];
   level++;
-  var randomNumber = Math.floor(Math.random()*4);
+  $("#title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomColour = buttonColours[randomNumber];
   gamePattern.push(randomColour);
 
-  $("#"+randomColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#"+ randomColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playsound(randomColour);
 
 }
+ function animatepress(currentColor){
+
+  $("#" + currentColor).addClass("pressed");
+   setTimeout(function(){
+    $("#" + currentColor).removeClass("pressed");
+  },100);
+ }
+
+ 
  function playsound(name){
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
  }
 
- function animatepress(currentColour){
+ function startover(){
 
-  $("#" +currentColour).addclass("pressed");
-   setTimeout(function(){
-    $("#" +currentColour).removeclass("pressed");
-  },100);
+  level = 0;
+  gamePattern = [];
+  started = false;
+
  }
